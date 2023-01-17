f1e71c2885750d1ce62c1575c2bdacc720086f3c
Béryl HOUESSOU <berylhoe@gmail.com>
2023-01-17 13:05:32 +0100

    Implement MinHeap from scratch in java 🥲

---
+++ src/main/kotlin/HeapDataStructure/MinHeap.java
@@ -0,0 +1,84 @@
+package HeapDataStructure;
+
+public class MinHeap {
+    private int[] Heap;
+    private int size;
+    private int maxSize;
+
+    public MinHeap(int size) {
+        this.Heap = new int [size+1];
+        this.maxSize = size;
+        this.size = 0;
+        this.Heap[0] = Integer.MIN_VALUE;
+    }
+    public int getParent (int position) {
+        return (position / 2);
+    }
+    public int leftChild(int position) {
+        return (2 * position);
+    }
+    public int rightChild(int position) {
+        return (2 * position) + 1;
+    }
+    public void swap(int firstPosition, int secondPosition) {
+        int tmp = Heap[firstPosition];
+        Heap[firstPosition] = Heap[secondPosition];
+        Heap[secondPosition] = tmp;
+    }
+    public void heapifyUp(int position) {
+        while (position > 1 && Heap[position] < Heap[getParent(position)]) {
+            this.swap(position, getParent(position));
+            position = getParent(position);
+        }
+    }
+    public void insert(int key) {
+        if (this.size < this.maxSize) {
+            Heap[++this.size] = key;
+            heapifyUp(this.size);
+        } else {
+            System.out.println("The Heap is full");
+        }
+    }
+    public void heapifyDown(int position){
+        this.swap(position, this.size);
+        while (position <= getParent(this.size) && (this.Heap[position] > this.Heap[rightChild(position)] || this.Heap[position] > this.Heap[leftChild(position)])){
+            int maxChild = Math.max(this.Heap[rightChild(position)],this.Heap[leftChild(position)]);
+            if (maxChild == this.Heap[rightChild(position)]) {
+                this.swap(position, rightChild(position));
+                position = rightChild(position);
+            } else {
+                this.swap(position, leftChild(position));
+                position = leftChild(position);
+            }
+        }
+    }
+    public int extract_min() {
+        int min = this.Heap[1];
+        this.Heap[1] = this.Heap[this.size--];
+        heapifyDown(1);
+        return min;
+    }
+
+    public void print() {
+        for (int i=1; i<=getParent(this.size); i++) {
+            try {
+                System.out.println(this.Heap[i] + "\n\t Left Child: " + this.Heap[leftChild(i)] + "\n\t Right Child: "+ this.Heap[rightChild(i)]);
+            } catch (Exception e) {
+                System.out.println(this.Heap[i] + "\n\t Left Child: " + this.Heap[leftChild(i)]);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+       MinHeap minHeap = new MinHeap(5);
+
+       minHeap.insert(50);
+       minHeap.insert(40);
+       minHeap.insert(10);
+       minHeap.insert(20);
+       minHeap.insert(30);
+
+       minHeap.print();
+       System.out.println(minHeap.extract_min());
+    }
+}
